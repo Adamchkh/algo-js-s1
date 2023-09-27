790610875a20d0b7258e2589c88a67620b5f6e89
Adamchkh <146080640+Adamchkh@users.noreply.github.com>
2023-09-27 10:58:42 +0200

    exercice 6 for each

--- exercice 6/main.js
+++ exercice 6/main.js
@@ -6,4 +6,9 @@ for (let i = 0; i < names.length; i++) {
     names[i] = names[i] + " va à la pèche "
     console.log(names[i])
 }
+names.forEach( name => {
+    name += " et au tenis"
+    console.log(name)
+
+})
 
