de8bb2b2919b7cce4b42c48588f71db59f9e5eb7
Adamchkh <146080640+Adamchkh@users.noreply.github.com>
2023-09-27 11:21:27 +0200

    exercice 7

---
+++ exercice 7/main.js
@@ -0,0 +1,7 @@
+let southparkCharacter = {
+firstName :"eric" ,
+lastName: "Cartman",
+Age: 10,
+Sentence: "Eric Cartman aime insulter les gens "
+}
+console.log (southparkCharacter.firstName,southparkCharacter.lastName,southparkCharacter.Age,southparkCharacter.Sentence)
