5abb72896fa92e27da313ebf4d8f22b95b47f291
Adamchkh <146080640+Adamchkh@users.noreply.github.com>
2023-09-27 09:10:26 +0200

    exercice 5
    5

---
+++ main.js
@@ -0,0 +1,13 @@
+var a = "jean"
+var b = "paul" 
+var result = checkName( a, b) 
+
+function checkName (userName1,userName2){
+if (userName1 == userName2){
+return true
+
+}
+else 
+return false 
+}
+console.log (result) 
