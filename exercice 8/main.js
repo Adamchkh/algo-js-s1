448e42b1684135d9dbdb573670d11bfea527b29f
Adamchkh <146080640+Adamchkh@users.noreply.github.com>
2023-09-28 18:40:55 +0200

    exercice 8
    8

---
+++ exercice 8/main.js
@@ -0,0 +1,53 @@
+class Pokemon {
+    constructor(name, attack, defense, hp, luck) {
+      this.name = name;
+      this.attack = attack;
+      this.defense = defense;
+      this.hp = hp;
+      this.luck = luck;
+    }
+  
+    isLucky() {
+      return Math.random() < this.luck / 100;
+    }
+  
+    attackPokemon(target) {
+      if (this.isLucky()) {
+        const damage = this.attack - target.defense;
+        if (damage > 0) {
+          target.hp -= damage;
+          console.log(`${this.name} attaque ${target.name} et inflige ${damage} points de dégâts.`);
+        } else {
+          console.log(`${this.name} attaque ${target.name} mais ne fait aucun dégât.`);
+        }
+      } else {
+        console.log(`${this.name} attaque ${target.name} mais rate.`);
+      }
+    }
+  }
+  
+  // Création de deux instances de Pokémon
+  const pikachu = new Pokemon('Pikachu', 25, 10, 100, 80);
+  const salameche = new Pokemon('Salameche', 30, 8, 100, 75);
+  
+  // Boucle de combat
+  while (pikachu.hp > 0 && salameche.hp > 0) {
+    // Pikachu attaque Salameche
+    pikachu.attackPokemon(salameche);
+    console.log(`${salameche.name} a ${salameche.hp} points de vie restants.`);
+  
+    if (salameche.hp <= 0) {
+      console.log(`${salameche.name} est KO. ${pikachu.name} est le vainqueur !`);
+      break;
+    }
+  
+    // Salameche attaque Pikachu
+    salameche.attackPokemon(pikachu);
+    console.log(`${pikachu.name} a ${pikachu.hp} points de vie restants.`);
+  
+    if (pikachu.hp <= 0) {
+      console.log(`${pikachu.name} est KO. ${salameche.name} est le vainqueur !`);
+      break;
+    }
+  }
+  
